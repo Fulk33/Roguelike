2a094fe6dd983aeb1be2c1d0fbe10eacda96e958
Fulk33 <spaceshipwars@gmx.de>
2015-11-17 17:22:39 +0100

    updated main

--- src/main.c
+++ src/main.c
@@ -8,8 +8,6 @@ int main(int argc, char const *argv[])
 	initscr();
 	noecho();
   	curs_set(FALSE);
-  	
-  	
 
   	Player* p = (Player*)malloc(sizeof(Player));
   	p->x = 5;
@@ -18,6 +16,8 @@ int main(int argc, char const *argv[])
 
   	mvaddch(p->x, p->y, p->symbol);
   	refresh();
+
+  	getch();
 
   	endwin();
 	return 0;
