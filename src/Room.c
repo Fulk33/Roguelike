25e9f10cc0984de5434ffde2fcd82b03b2a307c0
Fulk33 <spaceshipwars@gmx.de>
2015-11-17 21:03:51 +0100

    Added Wall detection to the drawRoom Function

--- src/Room.c
+++ src/Room.c
@@ -21,6 +21,13 @@ int deleteRoom(Room* room) {
 int drawRoom(Room* room) {
 	for (int i = 0; i < room->width; i++) {
 		for (int j = 0; j < room->height; j++) {
+			if((i==0 && j==0) || (i==0 && j==room->height-1) || (i==room->width-1 && j==0) || (i==room->width-1 && j==room->height-1)){
+				mvaddch(room->x+j, room->y+i, '+');
+			} else if(i==0 || i == room->width-1){
+				mvaddch(room->x+j, room->y+i, '|');
+			} else if(j==0 || j == room->height-1){
+				mvaddch(room->x+j, room->y+i, '-');
+			} else
 			mvaddch(room->x+j, room->y+i, '.');
 		}
 	}
