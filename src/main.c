aee4a14d0c5145331c9092a4d2162d88064fe261
Fulk33 <spaceshipwars@gmx.de>
2015-11-17 17:18:04 +0100

    updated main

--- src/main.c
+++ src/main.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ncurses.h>
+#include "../include/Player.h"
 
 int main(int argc, char const *argv[])
 {
 	initscr();
-	  noecho();
+	noecho();
   	curs_set(FALSE);
-  	//sleep(1);
+  	
+  	
+
+  	Player* p = (Player*)malloc(sizeof(Player));
+  	p->x = 5;
+  	p->y = 10;
+  	p->symbol = 'Q';
+
+  	mvaddch(p->x, p->y, p->symbol);
+  	refresh();
+
   	endwin();
 	return 0;
 }
