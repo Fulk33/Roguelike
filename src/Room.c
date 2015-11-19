f9950ad70894cb678b28abd2d6ae5cc8300972bf
Alepernicolo <NicoloPerna@ilpunto.net>
2015-11-19 21:26:32 +0100

    Added some Comments on Room.c

--- src/Room.c
+++ src/Room.c
@@ -1,8 +1,15 @@
 #include <stdlib.h>
 #include <ncurses.h>
 #include "../include/Room.h"
-
-Room* createRoom(int x, int y, int w, int h) {
+/**
+ * This will create a Room
+ * @param	x-coordinates in the window
+ * @param	y-coordinates in te window
+ * @param	width of the Room
+ * @param	height of te room
+ * @return	returns the Pointer newRoom
+ */			
+Room* createRoom(int x, int y, int w, int h) { 
 	Room* newRoom;
 	newRoom = (Room*)malloc(sizeof(Room));
 	newRoom->x = x;
@@ -12,12 +19,20 @@ Room* createRoom(int x, int y, int w, int h) {
 
 	return newRoom;
 }
-
+/**
+ * Deletes the Room
+ * @param	needs a room
+ * @return  returns 1
+ */
 int deleteRoom(Room* room) {
 	free(room);
 	return 1;
 }
-
+/**
+ * This function draws a room with the help of a 2-dimensional for-loop and some if-loops
+ * @param	needs a room
+ * @return 	returns 1
+ */
 int drawRoom(Room* room) {
 	for (int i = 0; i < room->width; i++) {
 		for (int j = 0; j < room->height; j++) {
